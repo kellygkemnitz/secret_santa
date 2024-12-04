706277131b1737a3955b86aa2de411d3dc110f4d
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-12-04 08:36:42 -0600

    2nd commit

--- secret_santa.py
+++ secret_santa.py
@@ -2,26 +2,27 @@ import random
 
 def secret_santa(participants):
     names = list(participants)
-    random.shuffle(names)
+    shuffled_names = sorted(names, key=lambda x: random.random())
     
     secret_santa_pairs = {}
     
-    for i in range(len(names)):
-        secret_santa_pairs[names[i]] = names[(i+1) % len(names)]
+    for i in range(len(shuffled_names)):
+        secret_santa_pairs[shuffled_names[i]] = shuffled_names[(i+1) % len(shuffled_names)]
 
     return secret_santa_pairs
 
-participants = {
-    'Garry',
-    'Denise',
-    'Kelly',
-    'Korey',
-    'Kameron',
-    'Christy',
-    'Cambri'
-}
+if __name__ == "__main__":
+    participants = {
+        'Garry',
+        'Denise',
+        'Kelly',
+        'Korey',
+        'Kameron',
+        'Christy',
+        'Cambri'
+    }
 
-pairs = secret_santa(participants)
+    pairs = secret_santa(participants)
 
-for giver, receiver in pairs.items():
-    print(f'{giver} --> {receiver}')
+    for giver, receiver in pairs.items():
+        print(f'{giver} --> {receiver}')
